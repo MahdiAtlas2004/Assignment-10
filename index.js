21d4d5e5130dbae4d747a7fa8516b923f98d790a
Mahdi Atlas <mahdiatlas2004@gmail.com>
2023-12-31 00:54:25 +0430

    getting values of inputs via dom

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function addContact(){
-    
+    var name = document.getElementById('name').value;
+      var lastName = document.getElementById('lastName').value;
+      var email = document.getElementById('email').value;
+      var phone = document.getElementById('phone').value;
+      var textarea = document.getElementById('textarea').value;
 }
