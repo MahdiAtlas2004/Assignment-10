585b940da6984cc2944f126f45fcfbe7c16452ff
Mahdi Atlas <mahdiatlas2004@gmail.com>
2023-12-31 00:58:34 +0430

    Using forEach function and display contacts function has been completed.

--- index.js
+++ index.js
@@ -29,4 +29,44 @@ function displayContacts() {
     var contacts = JSON.parse(localStorage.getItem('contacts')) || [];
 
     contacts.sort((a, b) => a.createdAt - b.createdAt);
+    contacts.forEach(function (contact, index) {
+        var contactDiv = document.createElement('div');
+        contactDiv.className = 'item';
+
+        // Update the class names to match your HTML
+        contactDiv.innerHTML = `
+            <div class="line">
+                <span class="before">First Name: </span>
+                <span class="fn">${contact.name}</span>
+                <hr>
+            </div>
+            <div class="line">
+                <span class="before">Last Name: </span>
+                <span class="ln">${contact.lastName}</span>
+                <hr>
+            </div>
+            <div class="line">
+                <span class="before">Phone Number: </span>
+                <span class="ph">${contact.phone}</span>
+                <hr>
+            </div>
+            <div class="line">
+                <span class="before">Email: </span>
+                <span class="em">${contact.email}</span>
+                <hr>
+            </div>
+            <div class="line message">
+                <span class="before beforemess">Message: </span>
+                <span class="mess">${contact.textarea}</span>
+                <hr>
+            </div>
+            <div class="line buttons">
+                <button onclick="editContact(${index})">Edit</button>
+                <button onclick="deleteContact(${index})">Delete</button>
+                <hr>
+            </div>
+        `;
+
+        contactList.appendChild(contactDiv);
+    });
 }
