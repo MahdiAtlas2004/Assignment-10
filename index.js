5ce45bd8aaddd3e51060bbe06e29615515cab56d
Mahdi Atlas <mahdiatlas2004@gmail.com>
2023-12-31 00:55:42 +0430

    addContact function has been completed.

--- index.js
+++ index.js
@@ -4,4 +4,21 @@ function addContact(){
       var email = document.getElementById('email').value;
       var phone = document.getElementById('phone').value;
       var textarea = document.getElementById('textarea').value;
-}
+      if (name && email) {
+        var contact = {
+          name: name,
+          lastName: lastName,
+          email: email,
+          phone: phone,
+          textarea: textarea,
+          createdAt: new Date().getTime()
+        };
+        var contacts = JSON.parse(localStorage.getItem('contacts')) || [];
+        contacts.push(contact);
+        localStorage.setItem('contacts', JSON.stringify(contacts));
+        displayContacts();
+        clearInputFields();
+      } else {
+        alert('Please enter both name and email');
+      }
+    }
